1e8703cf6475e74fe4f7a23abd89d81fef0142d3
tyler@dataondeck <49485904+dataondeck@users.noreply.github.com>
2019-12-04 15:49:18 -0500

    Update app.py

--- SemSegBuildings_Flask/app/app.py
+++ SemSegBuildings_Flask/app/app.py
@@ -1,4 +1,4 @@
-#import rastervision as rv
+import rastervision as rv
 import os 
 from flask import Flask, request
 
@@ -6,15 +6,14 @@ from flask import Flask, request
 # s3 uri
 #s3 = os.environ['$S3_BACKEND']
 # load up prediction package and tmp working directory
-#p = Predictor(s3, '/tmp')
+p = rv.Predictor('./predict_package.zip', '/tmp')
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def run():
     if request.method == 'GET':
-        return 'test'
+        return p
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host='0.0.0.0', port=5000)
