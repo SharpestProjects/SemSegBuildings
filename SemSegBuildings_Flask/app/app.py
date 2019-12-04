fd33b7047b228d463405a05335506ddaa5a0a2fd
dataondeck <tyler@dataondeck.com>
2019-12-04 18:39:23 +0000

    restructure

--- SemSegBuildings_Flask/app/app.py
+++ SemSegBuildings_Flask/app/app.py
@@ -1,12 +1,20 @@
-import rastervision as rv
+#import rastervision as rv
 import os 
+from flask import Flask, request
 
 
 # s3 uri
-s3 = os.environ['$S3_BACKEND']
+#s3 = os.environ['$S3_BACKEND']
 # load up prediction package and tmp working directory
-p = Predictor(s3, '/tmp')
+#p = Predictor(s3, '/tmp')
 
-app = Flask(__app__)
+app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def run():
+    if request.method == 'GET':
+        return 'test'
+
+if __name__ == '__main__':
+    app.run()
 
